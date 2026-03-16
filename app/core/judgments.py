a0bea56c4c853c21440d7019c04630f329737a95
Marco Viglianti <marcoviglianti@proton.me>
2026-03-16 19:50:45 +0100

    Create judgments.py

---
+++ app/core/judgments.py
@@ -0,0 +1,13 @@
+JUDGMENT_VALUES = {
+    "positive": 20,
+    "prev_positive": 10,
+    "prev_negative": -10,
+    "negative": -20,
+}
+
+JUDGMENT_LABELS_IT = {
+    "positive": "Positiva",
+    "prev_positive": "Prevalentemente positiva",
+    "prev_negative": "Prevalentemente negativa",
+    "negative": "Negativa",
+}
